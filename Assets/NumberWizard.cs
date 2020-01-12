ed04679e8115d02909e16780f7d0403a130498b3
chengze <ze.cheng@thoughtworks.com>
2020-01-12 17:53:18 +0800

    base commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Enter a number between 0-1000:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
