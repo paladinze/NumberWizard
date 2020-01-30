17e2133e55f12b27ad2d5230436fb27ff2637a6c
chengze <ze.cheng@thoughtworks.com>
2020-01-30 15:09:36 +0800

    added new fonts

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 15)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStoryText() {
+        return this.storyText;
+    }
+
+    public State[] GetNextStates() {
+        return this.nextStates;
+    }
+
+}
---
+++ Assets/Scripts/TankGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TankGame : MonoBehaviour
+{
+  [SerializeField] Text textComponent;
+  [SerializeField] State startState;
+
+  State currState;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    currState = startState;
+    textComponent.text = currState.GetStoryText();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ManageState();
+  }
+
+  private void ManageState()
+  {
+      var nextStates = currState.GetNextStates();
+      if (Input.GetKeyDown(KeyCode.Alpha1)) {
+          currState = nextStates[0];
+      }
+      if (Input.GetKeyDown(KeyCode.Alpha2)) {
+          currState = nextStates[1];
+      }
+      textComponent.text = currState.GetStoryText();
+  }
+}
